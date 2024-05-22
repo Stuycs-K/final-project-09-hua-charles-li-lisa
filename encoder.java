b45371465b96668ffbd994a835586b0c45d65533
Lisa Li <lli40@stuy.edu>
2024-05-22 14:50:01 -0400

    test files

---
+++ encoder.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class encoder{
+  public static void main (String[] args) throws FileNotFoundException{
+    
+  }
+}
