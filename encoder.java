0b5bee869b41d318f509d39c07e31fd03973597f
Charles Hua <chua40@stuy.edu>
2024-05-23 14:39:59 -0400

    Multiplication of the first entries of the matrices

--- encoder.java
+++ encoder.java
@@ -7,4 +7,13 @@ public class encoder{
   public static void main (String[] args) throws FileNotFoundException{
     
   }
+
+  public static encoded (String plaintext, int[][] key){
+    int[][] mat = 0;
+    for(i = 0; i < plaintexy.length; i++){
+      if (i % key.length = key.length - 1){
+        mat[i-key.length+1][1] = (plaintext.charAt(i-key.length + 1) - 65) * key[i - key.length + 1][i - key.length + 1];
+      }
+    }
+  }
 }
