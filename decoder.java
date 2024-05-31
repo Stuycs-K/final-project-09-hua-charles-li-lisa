275f6f47fe27c0ce3a7eb987b290249865f1439b
Lisa Li <lli40@stuy.edu>
2024-05-31 13:58:14 -0400

    2x2 matrix determinant

--- decoder.java
+++ decoder.java
@@ -24,7 +24,6 @@ public class decoder{
     skey.close();
     //System.out.println(keytext);
     if (keytext.length() == 9){
-      Scanner skey2 = new Scanner(keytext);
       int[][] key1 = new int[3][3];
       int row = 0;
       int col = 0;
@@ -38,13 +37,12 @@ public class decoder{
         }
         k++;
       }
-      skey2.close();
 
-      for (int i = 0; i < key1.length; i++){
+      /*for (int i = 0; i < key1.length; i++){
         for (int j = 0; j < key1[i].length; j++){
           System.out.println("key[" + i + "][" + j + "]: " + key1[i][j]);
         }
-      }
+      }*/
       System.out.println("");
       decoded3(ciphertext, key1);
 
@@ -63,17 +61,17 @@ public class decoder{
         k++;
       }
 
-      for (int i = 0; i < key1.length; i++){
+      /*for (int i = 0; i < key1.length; i++){
         for (int j = 0; j < key1[i].length; j++){
           System.out.println("key[" + i + "][" + j + "]: " + key1[i][j]);
         }
-      }
+      }*/
 
       System.out.println("");
+      decoded2(ciphertext, key1);
     }
 
   }
-
 
   public static void decoded3 (String ciphertext, int[][] key){
     boolean inverseM = false;
@@ -166,5 +164,93 @@ public class decoder{
         System.out.print((char) (plain[0][i] + 65));
       }
     }
+  }
+
+  public static void decoded2 (String ciphertext, int[][] key){
+    boolean inverseM = false;
+    int[][] mat = new int[1][2];
+    for (int i = 0; i < mat[0].length; i++){
+      mat[0][i] = ciphertext.charAt(i) - 65;
+      //System.out.println("mat[0][" + i + "]: " + mat[0][i]);
+    }
+    int det = key[0][0]*key[1][1] - key[0][1]*key[1][0];
+    //det = ad - bc
+    //System.out.println("");
+    System.out.println("Det: " + det);
+    if (det != 0){
+      System.out.println("There is an inverse.");
+      inverseM = true;
+    } else {
+      System.out.println("This is singular.");
+      inverseM = false;
+    }
+    //int[][] identity = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    //System.out.println("");
+    /*if (inverseM){
+      int[][] cofac = new int[3][3];
+      cofac[0][0] = key[1][1]*key[2][2] - key[1][2]*key[2][1];
+      cofac[0][1] = -1 * (key[1][0]*key[2][2] - key[1][2]*key[2][0]);
+      cofac[0][2] = key[0][1]*key[1][2] - key[0][2]*key[1][1];
+      cofac[1][0] = -1 * (key[0][1]*key[2][2] - key[0][2]*key[2][1]);
+      cofac[1][1] = key[0][0]*key[2][2] - key[0][2]*key[2][0];
+      cofac[1][2] = -1 * (key[0][0]*key[2][1] - key[0][1]*key[2][0]);
+      cofac[2][0] = key[1][0]*key[2][1] - key[1][1]*key[2][0];
+      cofac[2][1] = -1 * (key[0][0]*key[1][2] - key[0][2]*key[1][0]);
+      cofac[2][2] = key[0][0]*key[1][1] - key[0][1]*key[1][0];
+
+      int[][] inverse = new int[3][3];
+      inverse[0][0] = cofac[0][0];
+      inverse[0][1] = cofac[1][0];
+      inverse[0][2] = cofac[0][2];
+      inverse[1][0] = cofac[0][1];
+      inverse[1][1] = cofac[1][1];
+      inverse[1][2] = cofac[2][1];
+      inverse[2][0] = cofac[2][0];
+      inverse[2][1] = cofac[1][2];
+      inverse[2][2] = cofac[2][2];
+
+      for (int i = 0; i < inverse.length; i++){
+        for (int j = 0; j < inverse[0].length; j++){
+          if (inverse[i][j] < 0){
+            inverse[i][j] %= 26;
+            inverse[i][j] = 26 + inverse[i][j];
+          } else inverse[i][j] %= 26;
+          //System.out.println("inverse[" + i + "][" + j + "]: " + inverse[i][j]);
+        }
+      }
+
+      int midet = det % 26;
+      int a = midet;
+      while (true){
+        if ((a * midet) % 26 == 1){
+          midet = a;
+          break;
+        }
+        a++;
+      }
+      //System.out.println("Midet: " + midet);
+
+      for (int i = 0; i < inverse.length; i++){
+        for (int j = 0; j < inverse[0].length; j++){
+          inverse[i][j] *= midet;
+          inverse[i][j] = inverse[i][j] % 26;
+          //System.out.println("inverse[" + i + "][" + j + "]: " + inverse[i][j]);
+        }
+      }
+      //System.out.println("");
+
+      int[][] plain = new int[1][3];
+      plain[0][0] = inverse[0][0] * mat[0][0] + inverse[0][1] * mat[0][1] + inverse[0][2] * mat[0][2];
+      plain[0][1] = inverse[1][0] * mat[0][0] + inverse[1][1] * mat[0][1] + inverse[1][2] * mat[0][2];
+      plain[0][2] = inverse[2][0] * mat[0][0] + inverse[2][1] * mat[0][1] + inverse[2][2] * mat[0][2];
+      for (int i = 0; i < plain[0].length; i++){
+        plain[0][i] %= 26;
+        //System.out.println("plain[0][" + i + "]: " + plain[0][i]);
+      }
+      System.out.print("\nDecoded Message: ");
+      for (int i = 0; i < plain[0].length; i++){
+        System.out.print((char) (plain[0][i] + 65));
+      }
+    }*/
   }
 }
